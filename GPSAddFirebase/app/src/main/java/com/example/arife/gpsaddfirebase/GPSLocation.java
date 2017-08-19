34f633a4e49a65eaf5ad182e182428c8746c7d40
Arife Kubra <arfekubra@gmail.com>
2017-08-19 10:27:49 +0300

    Update GPSAddFirebase

--- GPSAddFirebase/app/src/main/java/com/example/arife/gpsaddfirebase/GPSLocation.java
+++ GPSAddFirebase/app/src/main/java/com/example/arife/gpsaddfirebase/GPSLocation.java
@@ -7,12 +7,9 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-
 
 import static android.content.Context.LOCATION_SERVICE;
 
@@ -21,6 +18,14 @@ import static android.content.Context.LOCATION_SERVICE;
  */
 
 public class GPSLocation implements LocationListener{
+
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+
+    private Location location;
     private LocationManager locationManager;
     private Context context;
 
@@ -30,22 +35,47 @@ public class GPSLocation implements LocationListener{
     }
 
     public Location getLocation() {
-        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        //check permissions are allowed.
+        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Log.e("error:","permissionda hata");
             return null;
         }
+
         try{
             locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+            //getting gps status
             boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-            if (isGPSEnabled) {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,6000,10,this);
-                Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                Log.d("loc",""+loc);
-                return loc;
-            }
-            else{
+            //getting network status
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            //none of provider enabled
+            if(!isNetworkEnabled&&!isGPSEnabled){
                 Log.e("error:","locationda");
             }
+            else{
+                // First get location from Network Provider
+                if(isNetworkEnabled){
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,MIN_DISTANCE_CHANGE_FOR_UPDATES,MIN_TIME_BW_UPDATES,this);
+                    if(locationManager != null){
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    }
+                    else{
+                        Log.e("Error:","location manager is null, network");
+                    }
+                }
+                //second gps enabled
+                if(isGPSEnabled){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,MIN_DISTANCE_CHANGE_FOR_UPDATES,MIN_TIME_BW_UPDATES,this);
+                    if(locationManager != null){
+                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    }
+                    else{
+                        Log.e("Error: ", "Location manager is null, gps");
+                    }
+                }
+
+            }
+
         }
         catch (Exception e){
             Log.e("error",""+e);
@@ -53,7 +83,7 @@ public class GPSLocation implements LocationListener{
 
         }
 
-        return null;
+        return location;
 
     }
 
@@ -78,5 +108,4 @@ public class GPSLocation implements LocationListener{
     public void onProviderDisabled(String s) {
 
     }
-
 }
